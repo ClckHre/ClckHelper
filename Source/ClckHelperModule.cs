29d37d181b0c8210ef0b94783970ad6861bd33a9
ClckHre <iwiswi.albir@gmail.com>
2026-01-03 13:54:53 +0100

    Initial commit

---
+++ Source/ClckHelperModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Celeste.Mod.ClckHelper;
+
+public class ClckHelperModule : EverestModule {
+    public static ClckHelperModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(ClckHelperModuleSettings);
+    public static ClckHelperModuleSettings Settings => (ClckHelperModuleSettings) Instance._Settings;
+
+    public override Type SessionType => typeof(ClckHelperModuleSession);
+    public static ClckHelperModuleSession Session => (ClckHelperModuleSession) Instance._Session;
+
+    public override Type SaveDataType => typeof(ClckHelperModuleSaveData);
+    public static ClckHelperModuleSaveData SaveData => (ClckHelperModuleSaveData) Instance._SaveData;
+
+    public ClckHelperModule() {
+        Instance = this;
+#if DEBUG
+        // debug builds use verbose logging
+        Logger.SetLogLevel(nameof(ClckHelperModule), LogLevel.Verbose);
+#else
+        // release builds use info logging to reduce spam in log files
+        Logger.SetLogLevel(nameof(ClckHelperModule), LogLevel.Info);
+#endif
+    }
+
+    public override void Load() {
+        // TODO: apply any hooks that should always be active
+    }
+
+    public override void Unload() {
+        // TODO: unapply any hooks applied in Load()
+    }
+}
---
+++ Source/ClckHelperModuleSession.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.ClckHelper;
+
+public class ClckHelperModuleSession : EverestModuleSession {
+
+}
